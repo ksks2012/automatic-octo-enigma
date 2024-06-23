9c6f5d57060561f62705fb6491eff08c5b5edae8
ksks2012 <st890751999@gmail.com>
2024-06-24 01:15:18 +0800

    feat(game_four): Implement game_four

--- bronze/4.cpp
+++ bronze/4.cpp
@@ -30,25 +30,25 @@ unordered_map<char, int> MOVE_MAP = {
     {'R', 3}
 };
 
-void game_one(string cur_gpu, int reg[], int tmp[]) {
+void game_one(string cur_gpu, int reg[], int move_weight[]) {
     // ignore when stun
     if(reg[3] > 0) {
         return;
     }
     if(reg[0] + 1 < cur_gpu.size() && cur_gpu[reg[0] + 1] == '#') {
-        tmp[0] += FIRST_PRIORITY;
-        tmp[1] += SECOND_PRIORITY;
-        tmp[2] += THIRD_PRIORITY;
-        tmp[3] += FOURTH_PRIORITY;
+        move_weight[0] += FIRST_PRIORITY;
+        move_weight[1] += SECOND_PRIORITY;
+        move_weight[2] += THIRD_PRIORITY;
+        move_weight[3] += FOURTH_PRIORITY;
     } else if(reg[0] + 2 < cur_gpu.size() && cur_gpu[reg[0] + 2] == '#') {
-        tmp[1] += FIRST_PRIORITY;
-        tmp[2] += SECOND_PRIORITY;
-        tmp[3] += THIRD_PRIORITY;
+        move_weight[1] += FIRST_PRIORITY;
+        move_weight[2] += SECOND_PRIORITY;
+        move_weight[3] += THIRD_PRIORITY;
     } else if(reg[0] + 3 < cur_gpu.size() && cur_gpu[reg[0] + 3] == '#') {
-        tmp[2] += FIRST_PRIORITY;
-        tmp[3] += SECOND_PRIORITY;
+        move_weight[2] += FIRST_PRIORITY;
+        move_weight[3] += SECOND_PRIORITY;
     } else {
-        tmp[3] += FIRST_PRIORITY;
+        move_weight[3] += FIRST_PRIORITY;
     }
 }
 
@@ -56,7 +56,7 @@ void game_two() {
 
 }
 
-void game_three(string cur_gpu, int reg[], int tmp[]) {
+void game_three(string cur_gpu, int reg[], int move_weight[]) {
     int risk = reg[3];
     if(risk < 0) {
         return;
@@ -67,37 +67,40 @@ void game_three(string cur_gpu, int reg[], int tmp[]) {
     // risk 1 -> step 3, 2, 1
     // risk 0 -> step 4, 3, 2, 1
     if(risk >= 3) {
-        tmp[MOVE_MAP[cur_gpu[0]]] += FIRST_PRIORITY;
+        move_weight[MOVE_MAP[cur_gpu[0]]] += FIRST_PRIORITY;
     } else if(risk >= 2) {
-        tmp[MOVE_MAP[cur_gpu[0]]] += SECOND_PRIORITY;
-        tmp[MOVE_MAP[cur_gpu[1]]] += FIRST_PRIORITY;
+        move_weight[MOVE_MAP[cur_gpu[0]]] += SECOND_PRIORITY;
+        move_weight[MOVE_MAP[cur_gpu[1]]] += FIRST_PRIORITY;
     } else if(risk >= 1) {
-        tmp[MOVE_MAP[cur_gpu[0]]] += THIRD_PRIORITY;
-        tmp[MOVE_MAP[cur_gpu[1]]] += SECOND_PRIORITY;
-        tmp[MOVE_MAP[cur_gpu[2]]] += FIRST_PRIORITY;
+        move_weight[MOVE_MAP[cur_gpu[0]]] += THIRD_PRIORITY;
+        move_weight[MOVE_MAP[cur_gpu[1]]] += SECOND_PRIORITY;
+        move_weight[MOVE_MAP[cur_gpu[2]]] += FIRST_PRIORITY;
     } else {
-        tmp[MOVE_MAP[cur_gpu[0]]] += FOURTH_PRIORITY;
-        tmp[MOVE_MAP[cur_gpu[1]]] += THIRD_PRIORITY;
-        tmp[MOVE_MAP[cur_gpu[2]]] += SECOND_PRIORITY;
-        tmp[MOVE_MAP[cur_gpu[3]]] += FIRST_PRIORITY;
+        move_weight[MOVE_MAP[cur_gpu[0]]] += FOURTH_PRIORITY;
+        move_weight[MOVE_MAP[cur_gpu[1]]] += THIRD_PRIORITY;
+        move_weight[MOVE_MAP[cur_gpu[2]]] += SECOND_PRIORITY;
+        move_weight[MOVE_MAP[cur_gpu[3]]] += FIRST_PRIORITY;
     }
 
     // just move by increment of risk
     // for(int i = 0; i < cur_gpu.size(); i++) {
     //     for(int j = 0; j < MOVE_SIZE; j++) {
     //         if(cur_gpu[i] == MOVE[j][0]) {
-    //             tmp[j] += FIRST_PRIORITY - i;
+    //             move_weight[j] += FIRST_PRIORITY - i;
     //         }
     //     }
     // }
     cerr << cur_gpu << endl;
     for(int i = 0; i < MOVE_SIZE; i++) {
-        cerr << tmp[i] << endl;
+        cerr << move_weight[i] << endl;
     }
 }
 
-void game_four() {
-
+void game_four(string cur_gpu, int reg[], int move_weight[], int turn) {
+    cerr << cur_gpu << endl;
+    cerr << turn << endl;
+    cerr << cur_gpu[0] << endl;
+    move_weight[MOVE_MAP[cur_gpu[0]]] += FIRST_PRIORITY;
 }
 
 int main()
@@ -107,6 +110,7 @@ int main()
     int nb_games;
     cin >> nb_games; cin.ignore();
 
+    int turn = 0;
     // game loop
     while (1) {
         for (int i = 0; i < 3; i++) {
@@ -127,18 +131,19 @@ int main()
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
 
-        int tmp[MOVE_SIZE] = {0};
+        int move_weight[MOVE_SIZE] = {0};
 
-        game_one(cur_gpu[0], reg[0], tmp);
-        // game_two(cur_gpu[1], reg[1], tmp);
-        game_three(cur_gpu[2], reg[2], tmp);
-        // game_four(cur_gpu[3], reg[3], tmp);
+        // game_one(cur_gpu[0], reg[0], move_weight);
+        // game_two(cur_gpu[1], reg[1], move_weight);
+        // game_three(cur_gpu[2], reg[2], move_weight);
+        game_four(cur_gpu[3], reg[3], move_weight, turn);
 
         for(int i = 0; i < MOVE_SIZE; i++) {
-            if(tmp[i] != 0) {
+            if(move_weight[i] != 0) {
                 cout << MOVE[i] << endl;
                 break;
             }
         }
+        turn++;
     }
 }
