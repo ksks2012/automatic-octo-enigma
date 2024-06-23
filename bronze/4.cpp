6cc03977514766fbac6a9557455c87ac71570cae
ksks2012 <st890751999@gmail.com>
2024-06-24 00:43:23 +0800

    feat(game_one): Using priority weights

--- bronze/4.cpp
+++ bronze/4.cpp
@@ -1,5 +1,5 @@
 /*
-    Jumping with majority rule
+    Move with Game 1, 3
 */
 
 #include <iostream>
@@ -17,21 +17,38 @@ using namespace std;
 #define MOVE_SIZE 4
 #define REG_SIZE 7
 
+#define FIRST_PRIORITY 4
+#define SECOND_PRIORITY 3
+#define THIRD_PRIORITY 2
+#define FOURTH_PRIORITY 1
+
 string MOVE[MOVE_SIZE] = {"UP", "LEFT", "DOWN", "RIGHT"};
+unordered_map<char, int> MOVE_MAP = {
+    {'U', 0},
+    {'L', 1},
+    {'D', 2},
+    {'R', 3}
+};
 
 void game_one(string cur_gpu, int reg[], int tmp[]) {
     // ignore when stun
     if(reg[3] > 0) {
-        continue;
+        return;
     }
     if(reg[0] + 1 < cur_gpu.size() && cur_gpu[reg[0] + 1] == '#') {
-        tmp[0]++;
+        tmp[0] += FIRST_PRIORITY;
+        tmp[1] += SECOND_PRIORITY;
+        tmp[2] += THIRD_PRIORITY;
+        tmp[3] += FOURTH_PRIORITY;
     } else if(reg[0] + 2 < cur_gpu.size() && cur_gpu[reg[0] + 2] == '#') {
-        tmp[1]++;
+        tmp[1] += FIRST_PRIORITY;
+        tmp[2] += SECOND_PRIORITY;
+        tmp[3] += THIRD_PRIORITY;
     } else if(reg[0] + 3 < cur_gpu.size() && cur_gpu[reg[0] + 3] == '#') {
-        tmp[2]++;
+        tmp[2] += FIRST_PRIORITY;
+        tmp[3] += SECOND_PRIORITY;
     } else {
-        tmp[3]++;
+        tmp[3] += FIRST_PRIORITY;
     }
 }
 
@@ -39,8 +56,8 @@ void game_two() {
 
 }
 
-void game_three() {
-
+void game_three(string cur_gpu, int reg[], int tmp[]) {
+    
 }
 
 void game_four() {
@@ -76,9 +93,9 @@ int main()
 
         int tmp[MOVE_SIZE] = {0};
 
-        game_one(cur_gpu[0], reg[0], tmp);
+        // game_one(cur_gpu[0], reg[0], tmp);
         // game_two(cur_gpu[1], reg[1], tmp);
-        // game_three(cur_gpu[2], reg[2], tmp);
+        game_three(cur_gpu[2], reg[2], tmp);
         // game_four(cur_gpu[3], reg[3], tmp);
 
         for(int i = 0; i < MOVE_SIZE; i++) {
