098bd4b87033e17dc69f17b52a4fa66a346c5663
ksks2012 <st890751999@gmail.com>
2024-06-24 00:16:52 +0800

    feat(3): Update input

--- wood/3.cpp
+++ wood/3.cpp
@@ -16,6 +16,7 @@ using namespace std;
 
 #define GPU_SIZE 30
 #define MOVE_SIZE 4
+#define REG_SIZE 7
 
 string MOVE[MOVE_SIZE] = {"UP", "LEFT", "DOWN", "RIGHT"};
 
@@ -33,21 +34,13 @@ int main()
             getline(cin, score_info);
         }
         string cur_gpu[nb_games];
-        int cur_pos[nb_games];
-        int stun[nb_games];
+        int reg[nb_games][REG_SIZE];
         for (int i = 0; i < nb_games; i++) {
-            string gpu;
-            int reg_0;
-            int reg_1;
-            int reg_2;
-            int reg_3;
-            int reg_4;
-            int reg_5;
-            int reg_6;
-            cin >> gpu >> reg_0 >> reg_1 >> reg_2 >> reg_3 >> reg_4 >> reg_5 >> reg_6; cin.ignore();
-            cur_gpu[i] = gpu;
-            cur_pos[i] = reg_0;
-            stun[i] = reg_3;
+            cin >> cur_gpu[i];
+            for(int j = 0; j < REG_SIZE; j++) {
+                cin >> reg[i][j];
+            }
+            cin.ignore();
         }
         
         // Write an action using cout. DON'T FORGET THE "<< endl"
@@ -56,14 +49,14 @@ int main()
         int tmp[MOVE_SIZE] = {0};
         for(int i = 0; i < nb_games; i++) {
             // ignore when stun
-            if(stun[i] > 0) {
+            if(reg[i][3] > 0) {
                 continue;
             }
-            if(cur_pos[i] + 1 < GPU_SIZE && cur_gpu[i][cur_pos[i] + 1] == '#') {
+            if(reg[i][0] + 1 < GPU_SIZE && cur_gpu[i][reg[i][0] + 1] == '#') {
                 tmp[0]++;
-            } else if(cur_pos[i] + 2 < GPU_SIZE && cur_gpu[i][cur_pos[i] + 2] == '#') {
+            } else if(reg[i][0] + 2 < GPU_SIZE && cur_gpu[i][reg[i][0] + 2] == '#') {
                 tmp[1]++;
-            } else if(cur_pos[i] + 3 < GPU_SIZE && cur_gpu[i][cur_pos[i] + 3] == '#') {
+            } else if(reg[i][0] + 3 < GPU_SIZE && cur_gpu[i][reg[i][0] + 3] == '#') {
                 tmp[2]++;
             } else {
                 tmp[3]++;
